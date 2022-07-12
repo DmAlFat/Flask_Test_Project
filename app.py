340a301dcb6fbb7ef573981f9993a1aa8065dc64
DmAlFat <fateev8292@gmail.com>
2022-07-12 22:33:52 +0300

    add test css bootstrap

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -9,6 +11,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/css')
+def css():
+    return render_template("css.html")
 
 @app.route('/test_list')
 def test_list():
